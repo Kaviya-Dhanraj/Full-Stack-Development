c8950b8b29ac5071dc9a112291ffa5d01e056939
Kaviya-Dhanraj <146418232+Kaviya-Dhanraj@users.noreply.github.com>
2023-10-17 22:15:28 +0530

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,7 @@
+function calculateBMI() {
+  let weight = Number(document.getElementById("weight").value);
+  let height = Number(document.getElementById("height").value);
+  let bmi = weight / (height * height);
+  let calculateBMI = document.getElementById("result");
+  calculateBMI.innerHTML = bmi.toFixed(2);
+}
